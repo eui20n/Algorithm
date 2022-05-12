6653803b75733ff59bf6306ad6081a02a553300e
eui20n <eui20n@naver.com>
2022-05-12 22:20:39 +0900

    2022-05-12

--- baejoon/4991번(로봇 청소기)/4991.py
+++ baejoon/4991번(로봇 청소기)/4991.py
@@ -4,12 +4,10 @@
     ----------------------------------------------
     <문제 설명>
     로봇 청소기가 더러운 칸을 깨끗하게 만드는게 필요한 이동 횟수가 몇번인가
-
     깨끗한 칸 : .
     더러운 칸 : * -> 10개가 넘지 않음
     가구 : x
     로봇 청소기의 시작 위치 : o -> 로봇 청소기는 무조건 한개 이다
-
     더러운 칸에 가면 q랑 방문처리 리스트 초기화 하기
 """
 from collections import deque
@@ -31,14 +29,15 @@ def cnt_dust():
     for x in range(R):
         for y in range(C):
             if dirty_home[x][y] == '*':
+                dirty_home[x][y] = str(2 ** cnt)
                 cnt += 1
 
     return cnt
 
 
-def visited_init():
+def visited_init(cnt):
     """ 방문 처리 리스트를 만들어줄 함수 """
-    visited = [[False] * C for _ in range(R)]
+    visited = [[[False] * C for _ in range(R)] for _ in range(2 ** cnt + 1)]
     return visited
 
 
@@ -50,45 +49,42 @@ def clean_house(x, y):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    visited = visited_init()
-    visited[x][y] = True
+    visited = visited_init(cnt)
+    visited[0][x][y] = True
 
     q = deque()
-    q.append([x, y, 0])
+    q.append([x, y, 0, {0}])  # x, y, 시간, 먹은 먼지 집합으로 표현
 
     while q:
-        x, y, time = q.popleft()
+        x, y, time, dust = q.popleft()
 
-        if cnt == 0:
+        if len(dust) == cnt + 1:
             return time
 
         for z in range(4):
             nx = x + dx[z]
             ny = y + dy[z]
-            if clean_con(nx, ny, visited):
+            if clean_con(nx, ny, visited, sum(dust)):
 
-                if dirty_home[nx][ny] == '*':
-                    cnt -= 1
-                    dirty_home[nx][ny] = '.'
+                if dirty_home[nx][ny].isnumeric():
+                    temp = int(dirty_home[nx][ny])
+                    visited[sum(dust | {temp})][nx][ny] = True
+                    q.append([nx, ny, time + 1, dust | {int(dirty_home[nx][ny])}])
+                    continue
 
-                    visited = visited_init()
-                    q = deque()
-
-                q.append([nx, ny, time + 1])
-                visited[nx][ny] = True
-
-                # print('x:', nx, 'y:', ny, 'time:', time)
+                q.append([nx, ny, time + 1, dust])
+                visited[sum(dust)][nx][ny] = True
 
     return -1
 
 
-def clean_con(x, y, visited):
+def clean_con(x, y, visited, dust):
     """ 조건 함수 """
     if 0 > x or x >= R:
         return False
     if 0 > y or y >= C:
         return False
-    if visited[x][y]:
+    if visited[dust][x][y]:
         return False
     if dirty_home[x][y] == 'x':
         return False
@@ -97,12 +93,12 @@ def clean_con(x, y, visited):
 
 while True:
     C, R = map(int, input().split())
+
     if R == 0 and C == 0:
         break
+
     dirty_home = [list(map(str, input())) for _ in range(R)]
 
     robot_cleaner = find_cleaner()
-    # print(robot_cleaner)
 
-    print(*dirty_home, sep = '\n')
     print(clean_house(robot_cleaner[0], robot_cleaner[1]))
