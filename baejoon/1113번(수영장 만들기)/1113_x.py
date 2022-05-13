5affa8b24608ddf33ed5d5a38ce3da4169265ba0
eui20n <eui20n@naver.com>
2022-05-13 10:10:07 +0900

    2022-05-13

--- baejoon/1113번(수영장 만들기)/1113_x.py
+++ baejoon/1113번(수영장 만들기)/1113_x.py
@@ -10,27 +10,67 @@
 from collections import deque
 
 R, C = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(C)]
+graph = [list(map(int, input())) for _ in range(R)]
+area = [[False] * C for _ in range(R)]
 
-def init_visited():
+
+def visited_init():
     """ 방문 처리 리스트를 만들어 주는 함수 """
-    visited = [[False]*C for _ in range(R)]
-    visited = side_change(visited)
+    visited = [[False] * C for _ in range(R)]
     return visited
 
-def side_change(arr):
-    """ 가장 자리를 True로 바꿔주는 함수 """
+
+def area_find():
+    """ 구역을 나눠주는 함수 """
+
+    cnt = 0
     for x in range(R):
-        arr[x][0] = True
-        arr[x][C-1] = True
-
-    for y in range(C):
-        arr[0][y] = True
-        arr[R-1][y] = True
-
-    return arr
+        for y in range(C):
+            if not area[x][y]:
+                cnt += 1
+                sep_area(x, y, cnt)
 
 
-# 생각이 난 풀이
-# 4방향을 조사해서 차이가 가장 큰 쪽으로 감, 이걸 옆으로 계속 반복하면 됨, 이렇게 차이가 구해진 것은 TRUE로 해서 다시는 안해지게 하면 됨
-# 반례 있나 생각해보기
+def sep_area(x, y, cnt):
+    """ 구역을 한 번 나누는 함수 """
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    visited = visited_init()
+
+    visited[x][y] = True
+    area[x][y] = cnt
+
+    q = deque()
+    q.append([x, y])
+
+    while q:
+        x, y = q.popleft()
+
+        for z in range(4):
+            nx = x + dx[z]
+            ny = y + dy[z]
+            if con_sep_area(nx, ny, x, y, visited):
+                q.append([nx, ny])
+                area[nx][ny] = cnt
+                visited[nx][ny] = True
+
+
+def con_sep_area(nx, ny, x, y, visited):
+    if 0 > nx or nx >= R:
+        return False
+    if 0 > ny or ny >= C:
+        return False
+    if visited[nx][ny]:
+        return False
+    if graph[x][y] != graph[nx][ny]:
+        return False
+    return True
+
+
+area_find()
+print(*area, sep='\n')
+
+# 임시 리스트를 만들고 임시 리스트에서 변경 값을 저장함, 그 후 원래 리스트와 임시 리스트의 차이를 구해서 더하면 됨
+# 이어져 있는지 확인하면 됨 -> 현재 기준에서 위와 아래로 같은지, 양 옆으로 같은지 확인하면 됨
+# 영역을 먼저 구하는게 맞을 듯
