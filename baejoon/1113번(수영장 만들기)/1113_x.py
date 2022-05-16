09f033cfc9f5f7ad766a706a3f3fc59803c1bf41
eui20n <eui20n@naver.com>
2022-05-16 19:44:34 +0900

    2022-05-16

--- baejoon/1113번(수영장 만들기)/1113_x.py
+++ baejoon/1113번(수영장 만들기)/1113_x.py
@@ -19,56 +19,116 @@ def visited_init():
     return visited
 
 
-def temp_init(layer):
-    """ 임시 리스트를 만들어 주는 함수 """
-    # 만약에 이거 없이 방문처리 리스트로 처리 가능하면 이거 뺄 것
-    temp = [[layer] * C for _ in range(R)]
-    return temp
+def find_min_max():
+    """ 최소값과 최대값을 찾아주는 함수 """
+    max_num = 0
+    min_num = float('inf')
 
-def cnt_water(layer):
-    """ 각층 마다 물이 얼마나 고이는지 확인하는 함수 """
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+    for x in range(R):
+        for y in range(C):
+            if graph[x][y] > max_num:
+                max_num = graph[x][y]
 
-    # 시작은 무조건 0,0에서 할 예정
-    visited = visited_init()
-    visited[0][0] = True
+            if graph[x][y] < min_num:
+                min_num = graph[x][y]
 
-    temp = temp_init(layer)
+    return [min_num, max_num]
+
+
+def make_pool(x, y, layer, visited):
+    """ 수영장을 만들어 주는 함수 """
+    # 잠길 곳이 없어서 0을 반환해줌
+    if graph[x][y] > layer:
+        visited[x][y] = True
+        return 0
+
+    if x == 0 or y == 0 or x == R - 1 or y == C - 1:
+        check_connect(x, y, visited, layer)
+        return 0
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    visited[x][y] = True
 
     q = deque()
-    q.append([0, 0])
-
-    cnt = 0
+    q.append([x, y])
+    result = 1
 
     while q:
         x, y = q.popleft()
+
         for z in range(4):
             nx = x + dx[z]
             ny = y + dy[z]
-            if cnt_water_con(nx, ny, visited, layer):
-                pass
+            if make_pool_con(nx, ny, visited, layer):
+                if nx == 0 or nx == R - 1 or ny == 0 or ny == C - 1:
+                    check_connect(nx, ny, visited, layer)
+                    return 0
+
+                q.append([nx, ny])
+                visited[nx][ny] = True
+                result += 1
+
+    return result
 
 
-def cnt_water_con(x, y, visited, layer):
-    """ cnt_water 조건 함수 """
+def make_pool_con(x, y, visited, layer):
+    """ make_pool의 조건 함수 """
     if 0 > x or x >= R:
         return False
     if 0 > y or y >= C:
         return False
     if visited[x][y]:
         return False
-    if graph[x][y] > layer: # 만약에 뭔가 이상하고 애매하면 이거 먼저 의심하기
+    if graph[x][y] > layer:
         return False
     return True
 
 
+def check_connect(x, y, visited, layer):
+    """ 가장 자리와 만나는 것이 뭔지 알려주는 함수 """
 
-# 먼저 물이 가장 높은 높이 까지 있다고 가정을 한고, 그 높이에서 흘러넘치는 물이 있는지 체크하고, 남은 물이 있으면 count한다 -> 이 방식을 1층까지 해서 총 남은 물의 양을 더한다
-# 아니면 1층 부터 높이 쌓아가는 방식도 있음 -> 우선 이 방식으로 하기
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-# 물이 고일 수 있는 조건
-# 1. 가장 자리이면 안된다.
-# 2. 둘러 쌓여 있어야 한다.
+    visited[x][y] = True
 
-# 가장 자리로 못가면 절대로 잠길 수 없음
+    q = deque()
+    q.append([x, y])
+
+    while q:
+        x, y = q.popleft()
+
+        for z in range(4):
+            nx = x + dx[z]
+            ny = y + dy[z]
+            if make_pool_con(nx, ny, visited, layer):
+                visited[nx][ny] = True
+                q.append([nx, ny])
+
+
+def main():
+    """ 함수를 실행 시키는 함수 """
+    cnt = 0
+
+    min_num, max_num = find_min_max()
+
+    for layer in range(min_num, max_num + 1):
+        visited = visited_init()
+
+        for x in range(R):
+            for y in range(C):
+                if not visited[x][y]:
+                    # print(layer)
+                    # print(*visited, sep = '\n')
+                    cnt += make_pool(x, y, layer, visited)
+                    # print('cnt', cnt)
+
+        # print('layer', layer)
+        # print('cnt', cnt)
+
+    return cnt
+
+
+print(main())
