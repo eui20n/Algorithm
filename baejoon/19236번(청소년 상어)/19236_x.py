eaaaf35a3da4d1bd7c854064969db6eed999af1c
eui20n <eui20n@naver.com>
2022-05-24 12:46:08 +0900

    2022-05-24

--- baejoon/19236번(청소년 상어)/19236_x.py
+++ baejoon/19236번(청소년 상어)/19236_x.py
@@ -1,44 +1,175 @@
-# 청소년 상어
-# 4 by 4 공간에 1 by 1인 정사각형 칸으로 나누어져 있다. 각 공간은 x,y로 표현가능하다
-# 한 칸에 물고기가 한 마리 존재하고, 물고기는 번호와 방향을 가지고 있다
-# 번호는 1 이상, 16이하의 자연수이며, 번호가 같은 물고기는 없다, 또한 방향은 상하좌우,대각선 8방향이며 한 방향을 갖는다
-# 청소년 상어는 (0,0)위치에 들어가고 (0,0)위치의 물고기를 먹는다
-# 상어의 방향은 먹은 물고기의 방향과 동일하다
-# 상어가 먹은 후 물고기가 이동을 한다
-# 물고기는 번호가 작은 물고기부터 순서대로 이동한다
-# 물고기는 한 칸을 이동할 수 있고, 상어가 있거나 범위를 벗어나는 값 때고는 다 이동가능 하다
-# 각 물고기는 이동할 수 있을때까지 45도 반시계 회전한다, 만약 이동할 수 있는 칸이 없으면 이동을 안한다
-# 그 외의 경우 이동을 하면 그 칸에 있는 물고기와 위치를 바꾼다
-# 물고기의 이동이 끝나면 상어가 방향의 칸으로 이동하며 여러칸 이동 가능하다
-# 이동하는 칸 중간에 있는 물고기는 먹지 않고, 물고기를 먹으면 그 물고기의 방향을 가지게 된다
-# 또한 물고기가 없는 칸으로는 이동할 수 없다
-# 상어가 이동할 수 있는 칸이 더이상 없으면 상어는 집으로간다
-# 상어가 이동할 칸이 없을때까지 이 과정을 반복한다
-# 상어가 먹을 수 있는 물고기의 최대값을 구하여라
-# 입력은 각 칸의 물고기의 번화 방향이 주어진다
-# 방향은 북(1) 북서(2) 서(3) 서남(4) 남(5) 남동(6) 동(7) 동북(8)
+"""
+    문제 이름 : 청소년 상어
+    URL : https://www.acmicpc.net/problem/19236
+    ----------------------------------------------
+    <문제 설명>
+    크기는 4 x 4 고정, 각 칸에 물고기가 방향과 번호를 가지고 있다 -> 1 <= 번호 <= 16, 번호가 같은 물고기는 없다
+    방향은 8방향 -> 상하좌우, 대각선
+    초기 위치 (0,0)
+    순서
+    1. 상어가 물고기를 먹는다 -> 상어는 먹은 물고기의 방향을 가지고 있다
+    2. 물고기가 이동한다 -> 한칸 이동
+    2-1. 번호가 작은 물고기 부터 순서대로 이동함, 이동은 빈 칸과 다른 물고기가 있는 칸, 이동 못하는 칸은 상어가 있거나 범위를 벗어나는 칸
+    2-2. 만약에 현재 방향에서 이동을 못하면 이동이 가능할때까지 45도 반시계 회전함, 그래도 이동할 칸이 없으면 이동 안함
+    2-3. 그 외 경우에는 이동하는데, 그 칸에 물고기와 자리를 바꾸는 방식으로 이동함
+    3. 상어 이동 -> 여러 칸 이동가능
+    3-1. 만약 물고기가 있는 칸으로 이동했으면 그 칸의 물고기를 먹고 그 방향을 얻음
+    3-2. 물고기가 없는 칸으로는 이동할 수 없다
+    3-3. 이동할 칸이 없으면 종료
 
-graph = []
-dir = []
+    상어가 먹을 수 있는 번호의 총합의 최대값을 구해라
 
-# 물고기의 크기와 상어가 있는 리스트와, 방향이 있는 리스트로 구분
+    북(1): x - 1, y
+    북서(2): x - 1, y - 1
+    서(3): x, y - 1
+    남서(4): x + 1, y - 1
+    남(5): x + 1, y
+    남동(6): x + 1, y + 1
+    동(7): x, y + 1
+    북동(8): x - 1, y + 1
+"""
+from copy import deepcopy
+
+fish_num = [[] for _ in range(4)]
+fish_dir_temp = [[] for _ in range(4)]
+
+for column in range(4):
+    a = list(map(int, input().split()))
+
+    for idx in range(len(a)):
+        if idx % 2 == 0:
+            fish_num[column].append(a[idx])
+
+        else:
+            fish_dir_temp[column].append(a[idx])
+
+fish_dir = [0] * 17
 for x in range(4):
-    a = list(map(int,input().split()))
-    graph.append([a[0],a[2],a[4],a[6]])
-    dir.append([a[1],a[3],a[5],a[7]])
-    
-# key - value 형태로 저장해서 사용하기 쉽게 만듬
-move_dir = {1:[-1,0], 2:[-1,-1], 3:[0,-1], 4:[1,-1], 5:[1,0], 6:[1,1], 7:[0,1], 8:[-1,1]}
+    for y in range(4):
+        fish_dir[fish_num[x][y]] = fish_dir_temp[x][y]
+
+shark_r = 0
+shark_c = 0
+shark_d = fish_dir[fish_num[0][0]]
+shark_score = fish_num[0][0]
+
+fish_dir[fish_num[0][0]] = 0
+fish_num[0][0] = -1
+
+global result
+result = 0
 
 
-visited = [[False for _ in range(4)] for _ in range(4)] # 이거 필요 없을 수도, 어차피 이동한 곳의 graph가 바뀌어서 방문처리 안해도 됨
+def fish_move(go_fish, fish_list, fish_dir_list):  # 이거 실행하면 됨
+    """ 물고기가 이동하는 함수 """
+    if go_fish == 17:
+        return
+    break_true = False  # 이거 어거지임, 이거 말고 다른 방법 있으면 그거 쓰기, 근데 이거 쓰면 맞긴함 -> 일단 맞추자..!
 
-def fish_move():
-    pass
+    #  생각할 것 -> 물고기가 없을 수도 있음
+    for x in range(4):
+        for y in range(4):
+            if fish_list[x][y] == go_fish:
+                # 물고기 방향보고 이동
+                change_fish_loc(x, y, go_fish, 1, fish_list, fish_dir_list)
+                break_true = True
+                break
+        if break_true:
+            break
 
-def dfs():
-    pass
-    
-# 생각할 것 -> 백트래킹 ==> 물고기가 이동한 후 이동하기 전으로 바꿀 수 있어야함
-# 순서 : 상어가 이동 -> 물고기 바꿈 ==> 만약 상어가 이동 후 갈곳이 없으면 바로 종료, 물고기 이동 안함
-    
+    fish_move(go_fish + 1, fish_list, fish_dir_list)  # 핵심은 이거 위치임, 이거 위치로 위의 반복문 전체가 멈춰야 함
+
+
+def change_fish_loc(x, y, fish, cnt, fish_list, fish_dir_list):
+    """ 물고기의 위치를 바꿔주는 함수 """
+    if cnt == 9:
+        return
+
+    dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
+    dy = [0, 0, -1, -1, -1, 0, 1, 1, 1]
+
+    go_fish_dir = fish_dir_list[fish]
+
+    # 재귀 들어가기
+    for z in range(1, 9):
+        if go_fish_dir == z:
+            nx = x + dx[z]
+            ny = y + dy[z]
+            if 0 <= nx < 4 and 0 <= ny < 4 and fish_list[nx][ny] != -1:
+                fish_list[nx][ny], fish_list[x][y] = fish_list[x][y], fish_list[nx][ny]
+                return
+
+            if go_fish_dir == 8:
+                fish_dir_list[fish] = 1
+            else:
+                fish_dir_list[fish] += 1
+
+            change_fish_loc(x, y, fish, cnt + 1, fish_list, fish_dir_list)
+
+
+def shark_move(x, y, d, fish_list, fish_dir_list, score):  # 상어 x, y, 방향, 물고기 리스트
+    """ 상어가 이동하는 함수 """
+    global result
+
+    # 여기서 재귀가 일어남
+    dx = [0, -1, -1, 0, 1, 1, 1, 0, -1]
+    dy = [0, 0, -1, -1, -1, 0, 1, 1, 1]
+    cnt = 1
+
+    if score > result:
+        result = score
+
+    print(result)
+
+    while True:
+        print(cnt)
+        nx = x + dx[d] * cnt
+        ny = y + dy[d] * cnt
+
+        if 0 > nx or nx >= 4 or 0 > ny or ny >= 4:
+            break
+
+        if 0 <= nx < 4 and 0 <= ny < 4 and fish_list[nx][ny] != 0:
+            score += fish_list[nx][ny]
+
+            fish_list[nx][ny] = -1
+            fish_list[x][y] = 0
+
+            d = fish_dir_list[fish_list[nx][ny]]
+            fish_dir_list[fish_list[nx][ny]] = 0
+
+            fish_list = deepcopy(fish_list)
+            fish_dir_list = deepcopy(fish_dir_list)
+
+            fish_move(1, fish_list, fish_dir_list)
+            shark_move(nx, ny, d, fish_list, fish_dir_list, score)
+
+        cnt += 1
+
+
+def main(fish_list, fish_dir_list):
+    """ 함수를 실행 시켜줄 함수 """
+
+    fish_list = deepcopy(fish_list)
+    fish_dir_list = deepcopy(fish_dir_list)
+
+    fish_move(1, fish_list, fish_dir_list)
+    shark_move(shark_r, shark_c, shark_d, fish_list, fish_dir_list, shark_score)
+
+# print(fish_dir)
+# print(*fish_num, sep='\n')
+
+
+main(fish_num, fish_dir)
+print(result)
+
+# 생각할것
+# 상어 위치를 어디에 담을지 생각하기
+# 번호가 겹치지 않으니 각각의 물고기의 방향은 딕셔너리에 담아서 관리할지 생각하기 -> 방향관리는 이걸로 하기
+# 상어의 현 위치는 -1, 물고기가 없는 곳은 0 으로 표시하기
+
+# 상어 이동 함수 안에 물고기 이동 함수가 있으면 됨 -> 재귀 호출
+# 방향 딕셔러니에서 리스트로 바꿀 것
+
+# 그냥 deepcopy 할지 말지 고민하기 -> 안할거면 고민을 좀 더 해야됨 -> 일단 deepcopy해서 답이 맞았는지 확인하기
+# 문제점 - 상어의 방향이 0이 되어서 while문이 안끝남
---
+++ baejoon/2933번(미네랄)/2933_x.py
@@ -0,0 +1,159 @@
+"""
+    문제 이름 : 미네랄
+    URL : https://www.acmicpc.net/problem/2933
+    ----------------------------------------------
+    <문제 설명>
+    왼쪽과 오른쪽에서 번갈아가며 진행됨 -> 왼쪽 먼저
+    막대나 미네랄을 만나면, 그 칸에 있는 미네랄은 모두 파괴되고 막대는 이동을 멈춤 -> 모두 파괴된다는게 그 칸에 있는 것을 의미 => 1개 파괴
+    파괴된 미네랄이 떠있으면 바닥으로 떨어지는데, 이때 본인 군집의 모양은 유지가 된다
+"""
+from collections import deque
+
+R, C = map(int, input().split())
+cave = [list(map(str, input())) for _ in range(R)]
+throw_count = int(input())
+bar_height = list(map(int, input().split()))
+
+
+def change_height():
+    """ 단위를 바꿔주는 함수로 높이가 아래에서 시작해서 헷갈림 """
+    for x in range(len(bar_height)):
+        bar_height[x] = R - bar_height[x]
+
+
+def visited_init():
+    """ 방문 처리 리스트를 만들어 주는 함수 """
+    visited = [[False] * C for _ in range(R)]
+    return visited
+
+
+def throw_bar(height, order):  # 높이와 오른쪽인지 왼쪽인지 순서 입력
+    """ 창을 던지는 함수 """
+    if order == 'L':
+        loc = 0
+        direction = 1
+
+    else:
+        loc = C - 1
+        direction = -1
+
+    while True:
+        if cave[height][loc] == 'x':
+            cave[height][loc] = '.'
+            break
+        loc += 1 * direction
+
+        if 0 > loc or loc >= C:
+            break
+
+
+def check_cluster(x, y, visited):
+    """ 군집이 있는지 확인해 주는 함수 """
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    visited[x][y] = True
+
+    q = deque()
+    q.append([x, y])
+
+    cluster = [[x, y]]
+
+    while q:
+        x, y = q.popleft()
+        for z in range(4):
+            nx = x + dx[z]
+            ny = y + dy[z]
+            if check_cluster_con(nx, ny, visited):
+                q.append([nx, ny])
+                visited[nx][ny] = True
+                cluster.append([nx, ny])
+
+    return cluster
+
+
+def check_cluster_con(x, y, visited):
+    """ check_cluter 의 조건 함수 """
+    if 0 > x or x >= R:
+        return False
+    if 0 > y or y >= C:
+        return False
+    if cave[x][y] == '.':
+        return False
+    if visited[x][y]:
+        return False
+    return True
+
+
+def find_floating_mineral():  # 군집 찾을 꺼면 이걸 실행 시키면 됨
+    """ 떠있는 미네랄을 찾아주는 함수 """
+    visited = visited_init()
+
+    # 바닥에 붙어 있는 것을 먼저 확인 -> 그 후 확인 하면 그건 떠있는 거임
+    for y in range(C):
+        if not visited[R - 1][y]:
+            check_cluster(R - 1, y, visited)
+
+    # 이제 전체 확인 -> 공중에 동시에 2개의 군집이 있을 수가 없어서 하나 찾으면 바로 종료
+    for x in range(R):
+        for y in range(C):
+            if cave[x][y] == '.':
+                continue
+
+            if not visited[x][y]:
+                floating_cluster = check_cluster(x, y, visited)
+                return floating_cluster
+
+    return []
+
+
+def gravity(floating_cluster):
+    """ 중력을 적용해주는 함수 """
+    # 한칸씩 내리면 될듯
+
+    floating_cluster.sort(key=lambda x: (x[1], -x[0]))
+    mineral = False
+
+    # 바꿔야함 -> floating_cluster는 안변하고 있음 -> 이거 고쳐야함, 여기만 고치면 맞음
+    while True:
+        for tmp in range(len(floating_cluster)):
+            x = floating_cluster[tmp][0]
+            y = floating_cluster[tmp][1]
+
+            if cave[x + 1][y] == 'x' or x + 1 == R - 1:
+                mineral = True
+
+            if cave[x + 1][y] == '.':
+                cave[x + 1][y] = cave[x][y]
+                cave[x][y] = '.'
+
+            floating_cluster[tmp] = [x + 1, y]
+
+        if mineral:
+            break
+
+
+def main():
+    """ 함수를 실행 시킬 함수 """
+    change_height()
+    cnt = 0
+    for x in bar_height:
+        if cnt % 2 == 0:
+            throw_bar(x, 'L')
+        else:
+            throw_bar(x, 'R')
+
+        cnt += 1
+
+        if floating_cluster := find_floating_mineral():
+            gravity(floating_cluster)
+
+    for x in cave:
+        print(''.join(x))
+
+
+main()
+
+# 해결해야 하는 것 -> 공중에 떠있는 상태
+# 중력 -> 모양이 안바뀌고 내려가야함
+# 동시에 2개의 군집이 떨어질 수 없음
